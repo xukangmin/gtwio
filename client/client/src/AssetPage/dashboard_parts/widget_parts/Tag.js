abbf52152f2d9b73443a2005b05f05b67bc62761
yunhan <yunhann@gmail.com>
2019-01-24 08:46:09 -0600

    update dashboard look

--- client/client/src/AssetPage/dashboard_parts/widget_parts/Tag.js
+++ client/client/src/AssetPage/dashboard_parts/widget_parts/Tag.js
@@ -5,37 +5,48 @@ import { Container, Row, Col, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { TempRadar } from './TempRadar';
 import { TempPlot } from './TempPlot';
 import { TempTable } from './TempTable';
+import { assetActions} from '../../../_actions/assetAction';
+import Loader from '../../../_components/loader';
 
 class Tag extends React.Component {
   constructor(props){
     super(props);
+    this.props.dispatch(assetActions.getSingleAssetData(JSON.parse(localStorage.getItem('user')),props.match.params.assetID));
 }
 
   render(){
+    const { assetData } = this.props;
     return(
       <div>
-        <Breadcrumb>
-          <BreadcrumbItem><a href="/">Home</a></BreadcrumbItem>
-          <BreadcrumbItem><a href="#">Asset: </a></BreadcrumbItem>
-          <BreadcrumbItem><a href="#">Tag: </a></BreadcrumbItem>
-        </Breadcrumb>
-        <Row>
-          <Col><TempRadar/></Col>
-          <Col><TempPlot/></Col>
-        </Row>
-        <Row>
-          <Col><TempTable/></Col>
-        </Row>
+        {assetData ?
+          <div>
+            <Breadcrumb>
+              <BreadcrumbItem><a href="/">Home</a></BreadcrumbItem>
+              <BreadcrumbItem><a href={"/asset/"+assetData.AssetID+"/dashboard"}>{assetData.DisplayName}</a></BreadcrumbItem>
+              <BreadcrumbItem><a href="#">Tag: </a></BreadcrumbItem>
+            </Breadcrumb>
+            <Row>
+              <Col><TempPlot/></Col>
+              <Col><TempRadar/></Col>
+            </Row>
+            <Row>
+              <Col><TempTable/></Col>
+              <Col></Col>
+            </Row>
+          </div>
+        :
+        <Loader/>
+      }
       </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-  // const { data } = state.dashboard;
-  return ({
-
-  });
+  const { data } = state.asset;
+  return {
+      assetData : data
+  };
 }
 
 const connectedPage = connect(mapStateToProps)(Tag);

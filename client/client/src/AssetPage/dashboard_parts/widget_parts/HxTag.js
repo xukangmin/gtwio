fe5eed6e1818e52c8344424ff775675f0710ad6d
yunhan <yunhann@gmail.com>
2019-01-31 09:50:10 -0600

    set tag plot interval

--- client/client/src/AssetPage/dashboard_parts/widget_parts/HxTag.js
+++ client/client/src/AssetPage/dashboard_parts/widget_parts/HxTag.js
@@ -13,7 +13,7 @@ class HxTag extends React.Component {
   constructor(props){
     super(props);
     this.props.dispatch(assetActions.getSingleAssetData(JSON.parse(localStorage.getItem('user')),props.match.params.assetID));
-    this.props.dispatch(dataActions.getSingleTagData(JSON.parse(localStorage.getItem('user')),props.match.params.assetID, props.match.params.tagID, Date.now()-600000, Date.now()));
+    this.props.dispatch(dataActions.getSingleTagData(JSON.parse(localStorage.getItem('user')),this.props.match.params.assetID, this.props.match.params.tagID, Date.now()-600000, Date.now()));
   }
 
   sortDevice(data){
@@ -32,6 +32,13 @@ class HxTag extends React.Component {
     ))
   }
 
+  componentDidUpdate(){
+    // this.interval = setInterval(() => {
+     // this.props.dispatch(dataActions.getSingleTagData(JSON.parse(localStorage.getItem('user')),this.props.match.params.assetID, this.props.match.params.tagID, Date.now()-600000, Date.now()));
+     // }, 5000);
+
+  }
+
   render(){
     const { AssetData } = this.props;
     let { DeviceData } = this.props;
@@ -47,12 +54,11 @@ class HxTag extends React.Component {
               <BreadcrumbItem><a href="#">{this.props.match.params.tagID}</a></BreadcrumbItem>
             </Breadcrumb>
             <Row>
-              <Col md="8"><TempPlot data={this.sortDevice(DeviceData)}/></Col>
-              <Col><TempRadar data={this.sortDevice(DeviceData)}/></Col>
+              <div className="col-9"><TempPlot asset={AssetData.AssetID} tag={this.props.match.params.tagID}/></div>
+              <div className="col-3"><TempRadar data={this.sortDevice(DeviceData)}/></div>
             </Row>
             <Row>
               <Col><TempTable data={this.sortDevice(DeviceData)}/></Col>
-              <Col></Col>
             </Row>
           </div>
         :
--- client/client/src/AssetPage/dashboard_parts/widget_parts/TempPlot.js
+++ client/client/src/AssetPage/dashboard_parts/widget_parts/TempPlot.js
@@ -1,47 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
+import Plot from 'react-plotly.js';
+import { dataActions } from '../../../_actions/dataAction';
 
 class TempPlot extends React.Component {
   constructor(props){
+
     super(props);
-
-    let temp = []
-    for (var i=0; i<this.props.data.length; i++){
-      temp.push({
-        x: this.props.data[i].Data.map((item,i)=>new Date(item.TimeStamp).toLocaleTimeString("en-US")),
-        y: this.props.data[i].Data.map((item,i)=>item.Value),
-        type: 'scatter',
-        name: this.props.data[i].SerialNumber
-      })
-    }
-
     this.state={
-      data: temp
-    }
-  }
-
-  plot() {
-      const layout = {
+      layout : {
         yaxis: {
           range: [0,100]
         }
-      }
-      Plotly.newPlot('plot', this.state.data, layout);
+      },
+      interval: 60
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  componentDidMount () {
-      this.plot();
+
+  handleChange(event) {
+    this.setState({interval: event.target.value});
+    // this.props.dispatch(dataActions.getSingleTagData(JSON.parse(localStorage.getItem('user')),this.props.asset, this.props.tag, Date.now()-this.state.interval*10000, Date.now()));
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
   }
 
   render(){
+    this.props.dispatch(dataActions.getSingleTagData(JSON.parse(localStorage.getItem('user')),this.props.asset, this.props.tag, Date.now()-this.state.interval*60*1000, Date.now()));
+    let { DeviceData } = this.props;
+    console.log("render")
+    let temp = []
+    for (var i=0; i<DeviceData.length; i++){
+      temp.push({
+        x: DeviceData[i].Data.map((item,i)=>new Date(item.TimeStamp).toLocaleTimeString("en-US")),
+        y: DeviceData[i].Data.map((item,i)=>item.Value),
+        type: 'scatter',
+        name: DeviceData[i].SerialNumber
+      })
+    }
+
     return(
-      <div id="plot"></div>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            {"Show  "}
+            <select value={this.state.interval} onChange={this.handleChange}>
+              <option value="10">10 minutes</option>
+              <option value="30">30 minutes</option>
+              <option value="60">60 minutes</option>
+            </select>
+          </label>
+          <input style={{display: "none"}} type="submit" value="Submit" />
+        </form>
+        <Plot
+            data={temp}
+            layout={this.state.layout}
+            style={{width:"100%"}}
+        />
+      </div>
     );
   }
 }
 
 function mapStateToProps(state) {
+  const device = state.data.data;
+
   return {
+      DeviceData: device
   };
 }
 

74c54bb53c3531db4aa974e0348246ce66d8f8c5
kangmin <kangmin@gtwlabs.com>
2019-05-03 08:30:41 -0500

    daq update

--- daq_script/daq_main.py
+++ daq_script/daq_main.py
@@ -134,9 +134,6 @@ class CoreModule:
             for i in ret:
                 if i[:2] != b'\xdb\x07':
                     cm_device_list.append(i[2:].decode())
-        
-        print("cm_list")
-        print(cm_device_list)
 
         for d in device_list:
             if d not in cm_device_list:
@@ -146,12 +143,6 @@ class CoreModule:
             if d not in device_list:
                 rm_list.append(d)
 
-        print("add_list")
-        print(add_list)
-        print("*********")
-        print("rm_list")
-        print(rm_list)
-
         for d in add_list:
             add_cmd = CMD_ADD_DAQ_SENSOR + d.encode()
             ret = self.SendCmdWithResponse(add_cmd)
@@ -160,15 +151,75 @@ class CoreModule:
             rm_cmd = CMD_RM_DAQ_SENSOR + d.encode()
             ret = self.SendCmdWithResponse(rm_cmd)
 
-    def sync_data(self, device_list, endpoint):
-        req = endpoint + '/device/getDeviceBySerialNumber?SerialNumber=02A050'
-        
-        # req = 'http://www.python.org/'
+    def sync_single_device(self, sn, endpoint):
+        req = endpoint + '/device/getDeviceBySerialNumber?SerialNumber=' + sn
         print(req)
-        with urllib.request.urlopen(req,timeout=100) as f:
-            buf = f.read(300).decode('utf-8')
-            print("GET RESPONSE")
-            print(buf)
+        try:
+            with urllib.request.urlopen(req,timeout=500) as f:
+                buf = f.read().decode('utf-8')
+        except:
+            print('cannot get data from endpoint, skip')
+            return
+
+        device_info = json.loads(buf)
+
+        if device_info['SerialNumber'] == sn: # match request
+            db_para = device_info['Parameters']
+            db_ts = 0
+            cm_ts = 0
+            for p in db_para:
+                if p['Type'] == 'Temperature':
+                    db_ts = p['CurrentTimeStamp']
+                    db_ts = int(db_ts / 1000)
+            print("db_ts=" + str(db_ts))
+            cm_cmd = CMD_READ_RECENT_DATA + sn.encode()
+            ret = self.SendCmdWithResponse(cm_cmd)
+            if len(ret) == 1 and len(ret[0]) == 14:
+                cm_ts = struct.unpack('<L',ret[0][6:10])[0]
+                print('cm_ts=' + str(cm_ts))
+            
+            if db_ts < cm_ts:
+                # potential data for update, read data from core module
+                cm_cmd = CMD_READ_BULK_DATA_BY_TIMESTAMP + sn.encode() + struct.pack('<L', db_ts + 1) + struct.pack('<L', cm_ts)
+                print(cm_cmd.hex())
+                ret = self.SendCmdWithResponse(cm_cmd)
+                print('data_size=' + str(len(ret) - 1))
+                header_size = struct.unpack('<L',ret[0][2:])[0]
+                print('header_size=' + str(header_size))
+                if len(ret) - 1 == header_size: # check size if match
+                    print('size match')
+                    for data in ret:
+                        if len(data) == 12:
+                            data_index = struct.unpack('<L', data[:4])[0]
+                            data_ts = struct.unpack('<L', data[4:8])[0]
+                            data_value = struct.unpack('<f', data[8:])[0]
+
+                            data_ts = data_ts * 1000 # convert to ms since cloud all use ms format
+                            data_value = data_value * 1.8 + 32 # convert from C to F
+
+                            print("index=" + str(data_index) + ",ts=" + str(data_ts) + ",value=" + str(data_value))
+                            # push data to the cloud / local end point
+                            push_data = {"SerialNumber": sn, "Value": data_value, "DataType": "Temperature", "TimeStamp": data_ts}
+                            push_params = json.dumps(push_data).encode('utf8')
+                            push_url = endpoint + '/data/addDataBySerialNumber'
+                            req = urllib.request.Request(push_url,data=push_params,headers={'content-type': 'application/json'})
+                            try:
+                                with urllib.request.urlopen(req,timeout=500) as f:
+                                    buf = f.read().decode('utf-8')
+                            except:
+                                print('push error happened, skip')
+                            
+        # except:
+        #     print("no sensor found or other error")
+        
+
+    def sync_data(self, device_list, endpoint):
+        print(device_list)
+
+        for d in device_list:
+            print(d)
+            self.sync_single_device(d, endpoint)
+
 
 
 with open('daq_config.json') as data_file:
@@ -178,21 +229,26 @@ local_end_point = config_data['LocalEndPoint']
 cloud_end_point = config_data['CloudEndPoint']
 core_modules = config_data['CoreModules']
 
-for single_cm in core_modules:
-    devices = single_cm['Devices']
-    mac_address = single_cm['MacAddress']
-    cm = CoreModule(mac_address)
-    cm.cm_connect()
-    # if internet():
-        # sync time stamp
-        # cm.sync_time()    
-        
-    # sync i
+while True:
+    for single_cm in core_modules:
+        devices = single_cm['Devices']
+        mac_address = single_cm['MacAddress']
+        cm = CoreModule(mac_address)
+        cm.cm_connect()
+        if internet():
+            # sync time stamp
+            cm.sync_time()    
+            
+        # sync interval
 
-    # daq_interval = single_cm['Interval']
+        daq_interval = single_cm['Interval']
 
-    # cm.sync_interval(daq_interval)
+        cm.sync_interval(daq_interval)
 
-    # cm.sync_device(single_cm['Devices'])
-    cm.sync_data(single_cm['Devices'], local_end_point)
-    cm.cm_disconnect()
+        cm.sync_device(single_cm['Devices'])
+        cm.sync_data(single_cm['Devices'], local_end_point)
+        cm.cm_disconnect()
+
+    time.sleep(60)
+
+
